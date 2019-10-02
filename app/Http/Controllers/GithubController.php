c18b88b165a513308d4fd2553f8a6d1d8966ac0f
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2019-10-02 16:37:19 +0530

    Using Guzzle, instead of php cURL methods, and refactoring

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 class GithubController extends Controller
 {
@@ -15,10 +17,14 @@ class GithubController extends Controller
     private $owner = 'konnector-dev';
     private $repo = 'kode';
     private $isPostJson = false;
+    private $http;
 
-    public function __construct()
+    public function __construct(Request $request)
     {
+        $this->requestData = $request->all();
         $this->setCurlGithubHeaders('User-Agent: jdecode');
+        $this->accessTokenChecker();
+        $this->http = new Client();
     }
 
     private function accessTokenChecker()
@@ -26,9 +32,10 @@ class GithubController extends Controller
         if (!isset($this->requestData['access_token'])) {
             return ['error' => 'Access token is required'];
         }
-        if (strlen($this->requestData['access_token'])) {
+        if (!strlen($this->requestData['access_token'])) {
             return ['error' => 'Empty access token'];
         }
+        $this->curlGithubToken = $this->requestData['access_token'];
     }
 
     public function sendToGithubAuth()
@@ -120,11 +127,15 @@ class GithubController extends Controller
 
     public function getSingleCommit(Request $request)
     {
-        $this->requestData = $request->all();
-        $this->accessTokenChecker($this->requestData);
-        $this->curlGithubToken = $this->requestData['access_token'];
-        $this->curlGithubUrl = "https://api.github.com/repos/{$this->owner}/{$this->repo}/commits/{$this->requestData['commit-sha']}";
-        return $this->curlGithub();
+        return $this->http->request(
+            'GET',
+            "https://api.github.com/repos/{$this->owner}/{$this->repo}/commits/{$this->requestData['commit-sha']}",
+            [
+                'headers' => [
+                    'Authorization' => "Bearer {$this->curlGithubToken}"
+                ]
+            ]
+        )->getBody();
     }
 
     public function setPushHooks(Request $request)
