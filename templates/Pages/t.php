b2f7153cff3abeacdb20fc7487c836e325820088
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2020-03-09 00:18:40 +0530

    Add Kode logo - looks awesome

--- templates/Pages/t.php
+++ templates/Pages/t.php
@@ -32,13 +32,14 @@ $this->disableAutoLayout();
         web - slackathon
     </title>
     <?= $this->Html->css('tailwind/style.css') ?>
-    <?php //echo $this->Html->meta('icon') ?>
+    <link rel="icon" type="image/png" href="/img/kode.png" />
 
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 
 </head>
 <body>
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.0.1/dist/alpine.js" defer></script>
+
 
 <div class="h-screen flex overflow-hidden bg-gray-100" x-data="{ sidebarOpen: false }" @keydown.window.escape="sidebarOpen = false">
     <!-- Off-canvas menu for mobile -->
@@ -53,8 +54,8 @@ $this->disableAutoLayout();
                 </button>
             </div>
             <div class="flex-shrink-0 flex items-center h-16 px-4 bg-gray-900">
-                Slackathon!
-                <!-- <img class="h-8 w-auto" src="/img/logos/workflow-logo-on-dark.svg" alt="Workflow" /> -->
+                 <img class="h-8 w-auto" src="/img/kode.png" alt="Kode" />
+                <span class="text-white mx-2">Kode!</span>
             </div>
             <div class="flex-1 h-0 overflow-y-auto">
                 <nav class="px-2 py-4">
@@ -103,8 +104,8 @@ $this->disableAutoLayout();
     <div class="hidden md:flex md:flex-shrink-0">
         <div class="flex flex-col w-64">
             <div class="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
-                Slackathon!
-                <!-- <img class="h-8 w-auto" src="/img/logos/workflow-logo-on-dark.svg" alt="Workflow" /> -->
+                 <img class="h-8 w-auto" src="/img/kode.png" alt="Kode" />
+                <span class="text-red-900 mx-2">Kode!</span>
             </div>
             <div class="h-0 flex-1 flex flex-col overflow-y-auto">
                 <!-- Sidebar component, swap this element with another sidebar if you like -->
